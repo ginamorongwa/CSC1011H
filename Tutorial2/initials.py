f4dd69cc804d6f66c4992a95196159e06a61b18c
Virginia Mthimunye <48602597+ginamorongwa@users.noreply.github.com>
2020-12-18 18:49:53 +0200

    Add initials.py

---
+++ Tutorial2/initials.py
@@ -0,0 +1,34 @@
+# xming
+# DISPLAY=localhost:0.0
+
+import turtle
+import time
+import math
+
+#screen = turtle.getscreen()
+t = turtle.Turtle()
+
+t.pencolor('red')
+t.hideturtle()
+t.right(90)
+t.forward(100)
+t.left(180)
+t.showturtle()
+t.color('red')
+t.forward(200)
+t.right(90)
+t.forward(100)
+t.right(90)
+t.forward(100)
+t.right(90)
+t.forward(100)
+t.left(135)
+t.forward(150)
+'''t.right(60)
+t.forward(100/math.cos(30))
+t.right(-120)
+t.forward(100/math.cos(30))
+t.right(-120)
+t.forward(100)'''
+
+time.sleep(20)
